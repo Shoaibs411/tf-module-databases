c5737f6bbd768fb21cdac36bde56630eef67a564
Shoaib <shoaibs411@gmail.com>
2024-01-05 02:25:59 +0530

    Provisioning DBS

--- docdb.tf
+++ docdb.tf
@@ -7,12 +7,12 @@ resource "aws_docdb_cluster" "docdb" {
 #   backup_retention_period = 5                    # Commented to avoid backups in Lab
 #   preferred_backup_window = "07:00-09:00"        # Commented to avoid backups in Lab
   skip_final_snapshot     = true
-  db_subnet_group_name =  ?
+  db_subnet_group_name    = aws_docdb_subnet_group.docdb.name 
 }
 
 
 # Creates a Subnet-Group
-resource "aws_docdb_subnet_group" "default" {
+resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-${var.ENV}-docdb-subnet-group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
