ddc5b8f98a6a2d4ce59f1410f23c597a2ca48314
Shoaib <shoaibs411@gmail.com>
2024-01-06 00:34:37 +0530

    Provisioning DB's - Assigning Security group to Cluster

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 # Creates Security Group for DocumentDB
 
-resource "aws_security_group" "docdb" {
+resource "aws_security_group" "allows_docdb" {
     name                    = "roboshop-${var.ENV}-docdb-security-group"
     description             = "roboshop-${var.ENV}-docdb-security-group"
     vpc_id                  = data.terraform_remote_state.vpc.outputs.VPC_ID
